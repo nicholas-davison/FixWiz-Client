0e6f10cb40a705a8a3200040f11d43d3aa9b732d
Nicholas Davison <nick.a.davison@gmail.com>
2024-07-24 12:03:48 -0500

    added error handling for unauth users trying to get tickets that dont belong to them

--- src/services/ServiceRequestService.jsx
+++ src/services/ServiceRequestService.jsx
@@ -9,12 +9,27 @@ export const getUserServiceRequests = async () => {
 
 
 export const getServiceRequestById = async (pk) => {
-  return await fetch(`http://localhost:8000/service_requests/${pk}`, {
-    headers: {
-        Authorization: `Token ${JSON.parse(localStorage.getItem("fix_token")).token}`
+  try {
+    const response = await fetch(`http://localhost:8000/service_requests/${pk}`, {
+      headers: {
+        Authorization: `Token ${JSON.parse(localStorage.getItem("fix_token")).token}`,
+        'Content-Type': 'application/json'
       }
-    }).then(res => res.json())
+    });
+    
+    if (response.status === 401) {
+      window.location.href = '/profile/service-requests';
+      alert('Nice Try! That"s not your ticket and you know it.');
+      return null;
+    }
+    return await response.json();
+
+  } catch (error) {
+    console.error('Network error or unexpected error:', error);
+    alert('An unexpected error occurred. Please try again later.');
+    return null;
   }
+};
 
 export const saveNewServiceRequest = async (newServiceRequestObj) => {
   return await fetch("http://localhost:8000/service_requests", {
