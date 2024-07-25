a08e93e24615648ee0d399797d6365aec1c4c540
Nicholas Davison <nick.a.davison@gmail.com>
2024-07-25 15:08:54 -0500

    get and set service requests

--- src/components/AllServiceRequests.jsx
+++ src/components/AllServiceRequests.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react"
 
 export const AllServiceRequests = () => {
 
+    const [allServiceRequests, setAllServiceRequests] = useState()
+
+    const getAndSetAllServiceRequests = () => {
+        
+    }
+
+    useEffect(() => {
+        getAndSetAllServiceRequests()
+    }, [])
     
     return (
         <>
